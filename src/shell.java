ec24bb3c4b729ab18b3fd8649e63340cbe274e81
Brianwastaken <mooprules@gmail.com>
2019-08-03 21:05:36 -0400

    shell data read.

--- src/shell.java
+++ src/shell.java
@@ -9,24 +9,34 @@ TASK: shell
 
 
 public class shell {
+    static int swaps;
+    static int[] A = new int[100];
+    static int[] B = new int[100];
+    static int[] G = new int[100];
+
         public static void main(String[] args) throws IOException {
             BufferedReader br = new BufferedReader(new FileReader("shell.in"));
             PrintWriter pw = new PrintWriter(new BufferedWriter(new FileWriter("shell.out")));
-            StringTokenizer st = new StringTokenizer(br.readLine());
 
-            int swaps = Integer.parseInt(br.readLine());
+            swaps = Integer.parseInt(br.readLine());
 
-            int[] A = new int[100];
-            int[] B = new int[100];
-            int[] G = new int[100];
-
-            for(int i = 0; i < swaps; i++) {
+            for (int i = 0; i < swaps; i++) {
+                StringTokenizer st = new StringTokenizer(br.readLine());
                 A[i] = Integer.parseInt(st.nextToken());
                 B[i] = Integer.parseInt(st.nextToken());
                 G[i] = Integer.parseInt(st.nextToken());
             }
 
-            int initPosi = 1;
+            int finalAnswer = 0;
+            for(int i = 1; i <= 3; i++) {
+                finalAnswer = Math.max(answer(i), finalAnswer);
+            }
+
+            pw.println(finalAnswer);
+            pw.close();
+        }
+
+        private static int answer(int initPosi) {
             int currentPosi = initPosi;
             int answer = 0;
             for(int i = 0; i < swaps; i++) {
@@ -40,8 +50,7 @@ public class shell {
 
                 }
             }
-            pw.println(answer);
-            pw.close();
+            return answer;
         }
     }
 
