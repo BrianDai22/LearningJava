ef14adafbeb13ccc8f90168f95c84d56676de8a1
Brianwastaken <mooprules@gmail.com>
2019-08-03 20:40:15 -0400

    shell data read.

---
+++ src/shell.java
@@ -0,0 +1,32 @@
+import java.io.*;
+import java.util.StringTokenizer;
+
+/*
+ID: mooprules1
+LANG: JAVA
+TASK: shell
+*/
+
+
+public class shell {
+        public static void main(String[] args) throws IOException {
+            BufferedReader br = new BufferedReader(new FileReader("shell.in"));
+            PrintWriter pw = new PrintWriter(new BufferedWriter(new FileWriter("shell.out")));
+            StringTokenizer st = new StringTokenizer(br.readLine());
+
+            int swaps = Integer.parseInt(br.readLine());
+
+            int[] A = new int[100];
+            int[] B = new int[100];
+            int[] G = new int[100];
+
+            for(int i = 0; i < swaps; i++) {
+                A[i] = Integer.parseInt(st.nextToken());
+                B[i] = Integer.parseInt(st.nextToken());
+                G[i] = Integer.parseInt(st.nextToken());
+            }
+
+
+        }
+    }
+
