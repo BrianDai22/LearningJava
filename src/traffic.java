6748c691f8e9659d4ce5ec61679adeb9c2a1a2d5
Brianwastaken <mooprules@gmail.com>
2019-09-14 20:24:52 -0400

    traffic thingy

--- src/traffic.java
+++ src/traffic.java
@@ -1,12 +1,14 @@
 import java.io.*;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class traffic {
     public static void main(String[] args) throws IOException {
         // Use BufferedReader rather than RandomAccessFile; it's much faster
-        BufferedReader f = new BufferedReader(new FileReader("revegetate.in"));
+        BufferedReader f = new BufferedReader(new FileReader("traffic.in"));
         // input file name goes above
-        PrintWriter out = new PrintWriter(new BufferedWriter(new FileWriter("revegetate.out")));
+        PrintWriter out = new PrintWriter(new BufferedWriter(new FileWriter("traffic.out")));
 
         int miles = Integer.parseInt(f.readLine());
         String[] ramps = new String[miles];
@@ -18,7 +20,13 @@ public class traffic {
             ramps[i] = st.nextToken();
             minCars[i] = Integer.parseInt(st.nextToken());
             maxCars[i] = Integer.parseInt(st.nextToken());
-            
+
         }
+        System.out.println(Arrays.toString(ramps));
+        System.out.println(Arrays.toString(minCars));
+        System.out.println(Arrays.toString(maxCars));
+
+        out.println();
+        out.close();
     }
 }
