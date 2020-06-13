2084353f02a8530541c839ac112939ad7eb071b8
Brianwastaken <mooprules@gmail.com>
2020-06-13 19:41:09 -0400

    dailytemp

--- src/cowJump.java
+++ src/cowJump.java
@@ -1,3 +1,9 @@
+import java.io.*;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.StringTokenizer;
+
 public class cowJump {
     static double evalBaseX;
 
@@ -5,9 +11,10 @@ public class cowJump {
         Long x;
         Long y;
         int segIndex;
+
         @Override
         public int compareTo(Point point) {
-            if(this.x.equals(point.x)) {
+            if (this.x.equals(point.x)) {
                 return this.y.compareTo(point.y);
             }
             return this.x.compareTo(point.x);
@@ -28,7 +35,7 @@ public class cowJump {
 
         @Override
         public int compareTo(Segement segement) {
-            if(this.index == segement.index) {
+            if (this.index == segement.index) {
                 return 0;
             }
             return 0;
@@ -41,6 +48,86 @@ public class cowJump {
         }
     }
 
+    public static int orientation(Point p1, Point p2, Point p3) {
 
+        long val = (p2.y - p1.y) * (p3.x - p2.x) - (p2.x - p1.x) * (p3.y - p2.y);
 
+        if (val == 0) {
+            return 0;
+        }
+
+        return (val > 0) ? 1 : 2;
+    }
+
+    public static boolean checkIntersect(Segement s1, Segement s2) {
+        if (orientation(s1.p, s1.q, s2.p) != orientation(s1.p, s1.q, s2.q)
+                && orientation(s2.p, s2.q, s1.p) != orientation(s2.p, s2.q, s1.q)) {
+            return true;
+        }
+        return false;
+    }
+
+    public static void main(String[] args) throws IOException {
+        // Use BufferedReader rather than RandomAccessFile; it's much faster
+        BufferedReader f = new BufferedReader(new FileReader("cowjump.in"));
+        // input file name goes above
+        PrintWriter out = new PrintWriter(new BufferedWriter(new FileWriter("cowjump.out")));
+        int n = Integer.parseInt(f.readLine());
+        Point[] points = new Point[n * 2];
+        Segement[] segements = new Segement[n];
+
+        for (int i = 0; i < n; i++) {
+            StringTokenizer st = new StringTokenizer(f.readLine());
+            Long x1 = Long.parseLong(st.nextToken());
+            Long y1 = Long.parseLong(st.nextToken());
+            Long x2 = Long.parseLong(st.nextToken());
+            Long y2 = Long.parseLong(st.nextToken());
+            points[i * 2] = new Point(x1, y1, i);
+            points[i * 2 + 1] = new Point(x2, y2, i);
+            segements[i] = new Segement(points[i * 2], points[i * 2 + 1], i);
+        }
+        Arrays.sort(points);
+        Set<Segement> active = new HashSet<>();
+
+        for(Point p: points) {
+            Segement currentSeg = segements[p.segIndex];
+            if(active.contains(currentSeg)) {
+                active.remove(currentSeg);
+            } else {
+                active.add(currentSeg);
+            }
+        }
+        int ans1 = -1;
+        int ans2 = -1;
+
+        for (int i = 0; i < n; i++) {
+            for (int j = i + 1; j < n; j++) {
+                if (checkIntersect(segements[i], segements[j])) {
+                    ans1 = i;
+                    ans2 = j;
+                    break;
+                }
+            }
+            if (ans1 != -1) {
+                break;
+            }
+        }
+        int ans2IntersectAmount = 0;
+
+        for (int i = 0; i < n; i++) {
+            if (i == ans2) {
+                continue;
+            }
+            if (checkIntersect(segements[i], segements[ans2])) {
+                ans2IntersectAmount++;
+            }
+        }
+        if (ans2IntersectAmount > 1) {
+            out.println(ans2 + 1);
+        } else {
+            out.println(ans1 + 1);
+        }
+        out.close();
+
+    }
 }
--- src/cowJump2.java
+++ src/cowJump2.java
@@ -1,5 +1,62 @@
+import java.io.*;
+import java.util.StringTokenizer;
+
 public class cowJump2 {
     static double evalBaseX;
+
+    public static void main(String[] args) throws IOException {
+        // Use BufferedReader rather than RandomAccessFile; it's much faster
+        BufferedReader f = new BufferedReader(new FileReader("cowjump.in"));
+        // input file name goes above
+        PrintWriter out = new PrintWriter(new BufferedWriter(new FileWriter("cowjump.out")));
+        int n = Integer.parseInt(f.readLine());
+        Point[] points = new Point[n * 2];
+        Segement[] segements = new Segement[n];
+
+        for (int i = 0; i < n; i++) {
+            StringTokenizer st = new StringTokenizer(f.readLine());
+            Long x1 = Long.parseLong(st.nextToken());
+            Long y1 = Long.parseLong(st.nextToken());
+            Long x2 = Long.parseLong(st.nextToken());
+            Long y2 = Long.parseLong(st.nextToken());
+            points[i * 2] = new Point(x1, y1, i);
+            points[i * 2 + 1] = new Point(x2, y2, i);
+            segements[i] = new Segement(points[i * 2], points[i * 2 + 1], i);
+        }
+
+        int ans1 = -1;
+        int ans2 = -1;
+
+        for (int i = 0; i < n; i++) {
+            for (int j = i + 1; j < n; j++) {
+                if (checkIntersect(segements[i], segements[j])) {
+                    ans1 = i;
+                    ans2 = j;
+                    break;
+                }
+            }
+            if (ans1 != -1) {
+                break;
+            }
+        }
+        int ans2IntersectAmount = 0;
+
+        for (int i = 0; i < n; i++) {
+            if (i == ans2) {
+                continue;
+            }
+            if (checkIntersect(segements[i], segements[ans2])) {
+                ans2IntersectAmount++;
+            }
+        }
+        if (ans2IntersectAmount > 1) {
+            out.println(ans2 + 1);
+        } else {
+            out.println(ans1 + 1);
+        }
+        out.close();
+
+    }
 
     public static class Point {
         Long x;
@@ -26,6 +83,22 @@ public class cowJump2 {
         }
     }
 
+    public static int orientation(Point p1, Point p2, Point p3) {
 
+        long val = (p2.y - p1.y) * (p3.x - p2.x) - (p2.x - p1.x) * (p3.y - p2.y);
 
+        if (val == 0) {
+            return 0;
+        }
+
+        return (val > 0) ? 1 : 2;
+    }
+
+    public static boolean checkIntersect(Segement s1, Segement s2) {
+        if (orientation(s1.p, s1.q, s2.p) != orientation(s1.p, s1.q, s2.q)
+                && orientation(s2.p, s2.q, s1.p) != orientation(s2.p, s2.q, s1.q)) {
+            return true;
+        }
+        return false;
+    }
 }
