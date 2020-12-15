d44254981e89c9d8970274064f933b5a3609e85c
Brianwastaken <mooprules@gmail.com>
2020-12-14 21:24:07 -0500

    commit everything since moving pc

--- src/testing1234.java
+++ src/testing1234.java
@@ -1,19 +1,11 @@
+import java.text.DecimalFormat;
 import java.util.Arrays;
 
 public class testing1234 {
     public static void main(String[] args) {
-        int[][] queries = new int[][]{{1, 2, 100},{2, 5, 100},{3, 4 ,100}};
-        int n = 5;
-        int m = queries.length;
-        int[] answer = new int[n];
-        long answer2 = 0;
-
-        for (int i = 0; i < m; i++) {
-            for (int j = queries[i][0]-1; j < queries[i][1]; j++) {
-                answer[j] += queries[i][2];
-                answer2 = Math.max(answer2, answer[i]);
-            }
-        }
+        double area = Math.PI * Math.pow(12, 2);
+        DecimalFormat fmt = new DecimalFormat("0.###");
+        System.out.println(fmt.format(area));
 
     }
 }
